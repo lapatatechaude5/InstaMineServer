0e06e540432f9265a65b4c280c5d843198e9233e
la_patate_chaude <lusrox28908@gmail.com>
2024-08-16 22:19:02 +0200

    .

--- src/main/java/com/site999/Main.java
+++ src/main/java/com/site999/Main.java
@@ -1,4 +1,35 @@
 package com.site999;
 
-public class Main {
+import com.site999.Server.CreateServer;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("InstaMineServer");
+
+        Button createserveracceuil = new Button();
+        createserveracceuil.setText("Créer un serveur");
+        createserveracceuil.setOnAction(event -> {
+            CreateServer.create();
+        });
+
+        // Layout de base
+        StackPane root = new StackPane();
+        root.getChildren().add(createserveracceuil);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
---
+++ src/main/java/com/site999/Server/CreateServer.java
@@ -0,0 +1,8 @@
+package com.site999.Server;
+
+public class CreateServer {
+    public static void create() {
+        // Logique pour créer un serveur
+        System.out.println("Serveur Minecraft créé !");
+    }
+}
